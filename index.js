0a91555a0022c68550a68079d0ea3c3da61bde77
Daniel Teixeira da Silva <danieldts2013@gmail.com>
2020-04-20 17:47:17 -0300

    alterando estrutura de pastas para o heroku

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const express = require('express')
 const app = express();
 const docx = require('docx');
-const fs = require('fs');
+const dotenv = require('dotenv');
+//const fs = require('fs');
 const { Document, Packer, Paragraph, Table, TableCell, TableRow,TextRun  } = docx;
 //import * as docx from "docx";
 //import * as fs from "fs";
-
-//const express = require("express");
-//const app = express(exports);
-
+dotenv.config()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
-
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
-
+app.listen(process.env.PORT || 8000);
 app.get("/word", (req, res) => {
 
   console.log("executing....");
@@ -47,22 +41,26 @@ app.get("/word", (req, res) => {
   //  res.setHeader('Content-Disposition', 'attachment; filename=My Document.docx');
   //  res.send(Buffer.from(b64string, 'base64'));
   // Used to export the file into a .docx file
-  const b64string = Packer.toBase64String(doc);
+  const b64string = Packer.toBase64String(doc)
+  .then(function(result){
+    return result;
+  });
   //Packer.toBuffer(doc).then((buffer) => {
-//      fs.writeFileSync("My Document.docx", buffer);
-//  });
-    res.setHeader('Content-Disposition', 'attachment; filename=My Document.docx');
-    var doc64 = Buffer.from('base64',b64string,);
+  //fs.writeFileSync("My Document.docx", buffer);
+  //});
+  //res.send(Buffer.from(b64string, 'base64'));
+
+
+  res.setHeader('Content-Disposition', 'attachment; filename=My Document.docx');
+  var doc64 = Buffer.from('base64',b64string,);
     res.writeHead(200, {
     'Content-Type': 'application/octet-stream',
     'Content-Length': doc64.length
   });
   res.end(doc64);
-    //res.download('My Document.docx');
 
-    console.log('escreveu base 64')
-    //var files = fs.createReadStream("My Document.docx");
-    //res.writeHead(200, {'Content-disposition': 'attachment; filename=My Document.docx'}); //here you can add more headers
-    //files.pipe(res)
-
+  console.log('escreveu base 64');
+  //var files = fs.createReadStream("My Document.docx");
+  //res.writeHead(200, {'Content-disposition': 'attachment; filename=My Document.docx'}); //here you can add more headers
+  //files.pipe(res)
 });
